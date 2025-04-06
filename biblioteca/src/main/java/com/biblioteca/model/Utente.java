1e382d337bc761f39649989e143d5a162676652a
Isabella De Sanctis <desanctisisabella95@gmail.com>
2025-04-06 17:28:57 +0200

    Revert "Feature/creazione biblioteca utenti"

--- biblioteca/src/main/java/com/biblioteca/model/Utente.java
+++ biblioteca/src/main/java/com/biblioteca/model/Utente.java
@@ -1,48 +1,4 @@
 package com.biblioteca.model;
 
 public class Utente {
-
-    // variabili nome, cognome e idUtente
-    private static String nome;
-    private static String cognome;
-    private static int idUtente;
-
-    // costruttore getter e setter
-    public Utente (String nome, String cognome, int idUtente){
-        this.nome = nome;
-        this.cognome = cognome;
-        this.idUtente = idUtente;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public String getCognome() {
-        return cognome;
-    }
-
-    public void setCognome(String cognome) {
-        this.cognome = cognome;
-    }
-
-    public int getIdUtente() {
-        return idUtente;
-    }
-
-    public void setIdUtente(int idUtente) {
-        this.idUtente = idUtente;
-    }
-
-    // metodo per stampare i dettagli dell'utente
-    public static void dettagliUtente(){
-
-        System.out.println("Nome: " + nome);
-        System.out.println("Cognome: " + cognome);
-        System.out.println("ID Utente: " + idUtente);
-    }
 }
