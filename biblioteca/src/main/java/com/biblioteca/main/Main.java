c304b038cf368d9e1803adade5f89dcfef733ce2
Isabella De Sanctis <desanctisisabella95@gmail.com>
2025-04-07 15:37:27 +0200

    Update Main.java
    Modifica classe Main

--- biblioteca/src/main/java/com/biblioteca/main/Main.java
+++ biblioteca/src/main/java/com/biblioteca/main/Main.java
@@ -10,6 +10,12 @@ import com.biblioteca.model.Libro;
 public class Main {
     public static void main(String[] args){
 
+        Libro libro1 =new Libro("Quello che so di te", "Nadia Terranova", 2025,  9788823521234L);
+        Libro libro2 = new Libro("Fratellino" ,"di Ibrahima Balde e Amets",2025,  9788807895678L);
+        Libro libro3 = new Libro("Macroeconomia","N. Gregory Manki",2016, 9788880085096L);
+        Libro libro4 = new Libro("Il nome della rosa", " Umberto Eco", 1980,  9788845240000L);
+        Libro libro5 = new Libro( "Un mondo nuovo", "Liz Braswell" , 2015, 9781788107686L);
+        
         Biblioteca.aggiungi(libro1);
         Biblioteca.aggiungi(libro2);
         Biblioteca.aggiungi(libro3);
@@ -23,11 +29,7 @@ public class Main {
             
 
 
-        Libro libro1 =new Libro("Quello che so di te", "Nadia Terranova", 2025,  9788823521234L);
-        Libro libro2 = new Libro("Fratellino" ,"di Ibrahima Balde e Amets",2025,  9788807895678L);
-        Libro libro3 = new Libro("Macroeconomia","N. Gregory Manki",2016, 9788880085096L);
-        Libro libro4 = new Libro("Il nome della rosa", " Umberto Eco", 1980,  9788845240000L);
-        Libro libro5 = new Libro( "Un mondo nuovo", "Liz Braswell" , 2015, 9781788107686L);
+       
 
 
     }
