972db3de95aba0392e247ac2c5e870ec3484dfd1
desan <desanctisisabella95@gmail.com>
2025-05-19 18:04:15 +0200

    inserimento query creazione tabelle libri e utenti e inserimento valori

--- biblioteca/src/main/resources/db/data.sql
+++ biblioteca/src/main/resources/db/data.sql
@@ -1,1 +1,14 @@
 -- insert
+
+INSERT INTO biblioteca.libri (titolo, autore, anno_pubblicazione, isbn) VALUES
+('Quello che so di te', 'Nadia Terranova', 2025, 9788823521234),
+('Fratellino', 'Ibrahima Balde e Amets', 2025, 9788807895678),
+('Macroeconomia', 'N. Gregory Manki', 2016, 9788880085096),
+('Il nome della rosa', 'Umberto Eco', 1980, 9788845240000);
+
+INSERT INTO biblioteca.utenti (nome, cognome)
+VALUES ('Mario', 'Rossi'),
+('Sheldon', 'Cooper'),
+('Leonard', 'Hoffsteader'),
+('Bianca', 'Piazzolla'),
+('Mario', 'Draghi');
--- biblioteca/src/main/resources/db/schema.sql
+++ biblioteca/src/main/resources/db/schema.sql
@@ -1,1 +1,14 @@
 -- creazione db
+
+
+CREATE TABLE libri (
+titolo VARCHAR (100),
+autore VARCHAR (100),
+anno_pubblicazione INT,
+isbn VARCHAR (20) PRIMARY KEY);
+
+CREATE TABLE utenti (
+id_utente INT PRIMARY KEY AUTO_INCREMENT,
+nome VARCHAR(100),
+cognome VARCHAR (100)
+);
