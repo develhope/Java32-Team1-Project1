6acea8bd2a33663d9aa3857a8174354a6ee19df2
Luciano Addeo <lucianoaddeo7@gmail.com>
2025-05-21 18:58:25 +0200

    creazione classe prestitorepository vuota

---
+++ biblioteca/src/main/java/com/biblioteca/repository/PrestitoRepository.java
@@ -0,0 +1,13 @@
+package com.biblioteca.repository;
+
+import com.biblioteca.model.Libro;
+import com.biblioteca.model.Prestito;
+import com.biblioteca.model.Utente;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+//serve creare un metodo per la select tramite id di libro
+public class PrestitoRepository extends AbstractRepository {
+
+}
