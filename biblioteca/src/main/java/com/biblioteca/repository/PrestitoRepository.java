6d34a0c9c1faea42f06052c036508ae313080b82
Luciano Addeo <lucianoaddeo7@gmail.com>
2025-05-21 20:01:28 +0200

    risoluzione conflitti PrestitoRepository.java

--- biblioteca/src/main/java/com/biblioteca/repository/PrestitoRepository.java
+++ biblioteca/src/main/java/com/biblioteca/repository/PrestitoRepository.java
@@ -1,14 +1,24 @@
 package com.biblioteca.repository;
 
-import com.biblioteca.model.Libro;
 import com.biblioteca.model.Prestito;
 import com.biblioteca.model.Utente;
 
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-//serve creare un metodo per la select tramite id di libro
-public class PrestitoRepository extends AbstractRepository {
 
+public class PrestitoRepository  extends AbstractRepository{
 
+    public Prestito findById(int id) throws SQLException {
+        String queryFindById = "SELECT isbn, titolo, id_prestito, nome, cognome, u.id_utente data_prestito FROM prestiti AS p JOIN  utenti AS u ON p.id_utente = u.id_utente WHERE " + id + " id_prestito " ;
+//        SELECT id_prestito, nome, cognome, data_prestito FROM prestiti AS p JOIN  utenti AS u ON p.id_utente = u.id_utente WHERE  id_prestito = ?;
+        PreparedStatement statement = connection.prepareStatement(queryFindById);
+        ResultSet resultSet = statement.executeQuery();
+        if (resultSet.next()) {
+            return new Prestito(resultSet.getInt("id"), resultSet.getString("cognome"),id );
+        }
+        System.out.println(resultSet.next());
+
+        return null;
+    }
 }
