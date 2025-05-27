76df72e78824d7ccb71363d7138e8723b3ddcacf
Luciano Addeo <lucianoaddeo7@gmail.com>
2025-05-27 09:20:27 +0200

    conflitti PrestitoService PrestitoRepository

--- biblioteca/src/main/java/com/biblioteca/service/PrestitoService.java
+++ biblioteca/src/main/java/com/biblioteca/service/PrestitoService.java
@@ -7,15 +7,18 @@ import java.sql.SQLException;
 
 public class PrestitoService {
 
-   private PrestitoRepository prestitoRepository =  new PrestitoRepository();
-
-   public boolean create(Prestito p) throws SQLException {
-      int numeroCopie =  prestitoRepository.getNumeroCopieDisponibili(p.getLibro());
-       if(numeroCopie > 0) {
-           prestitoRepository.save(p);
-           return true;
-       }
-       else
-           return false;
-   }
+//    private PrestitoRepository prestitoRepository = new PrestitoRepository();
+//
+//    public boolean create(Prestito p) throws SQLException {
+//        int numeroCopie = prestitoRepository.getNumeroCopieDisponibili(p.getLibro());
+//
+//        if (numeroCopie > 0) {
+//            prestitoRepository.save(p);
+//            return true;
+//        } else {
+//            return false;
+//        }
+//    }
+    
 }
+
