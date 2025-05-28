5753b562f23a0135af60d4d75b0c09b1ef5f9683
Clarissa De Benedittis <debenedittisclarissa04@gmail.com>
2025-05-28 08:42:27 +0200

    aggiunti al database: 2 nuovi libri uno con 0 copie e l'altro con 1 copia, un nuovo utente che fa un solo prestito

--- biblioteca/src/main/resources/db/data.sql
+++ biblioteca/src/main/resources/db/data.sql
@@ -5,7 +5,9 @@ INSERT INTO biblioteca.libri (titolo, autore, anno_pubblicazione, isbn, numero_c
 ('Quello che so di te', 'Nadia Terranova', 2025, 9788823521234, 12),
 ('Fratellino', 'Ibrahima Balde e Amets', 2025, 9788807895678, 24),
 ('Macroeconomia', 'N. Gregory Manki', 2016, 9788880085096, 8),
-('Il nome della rosa', 'Umberto Eco', 1980, 9788845240000, 7);
+('Il nome della rosa', 'Umberto Eco', 1980, 9788845240000, 7),
+('It Ends With Us', 'Colleen Hoover', 2021, 9781501110368, 0),
+('Orgoglio e pregiudizio', 'Jane Austen', 1813, 9788817115061, 1);
 
 -- utenti
 INSERT INTO biblioteca.utenti (nome, cognome)
@@ -13,7 +15,8 @@ VALUES ('Mario', 'Rossi'),
 ('Sheldon', 'Cooper'),
 ('Leonard', 'Hoffsteader'),
 ('Bianca', 'Piazzolla'),
-('Mario', 'Draghi');
+('Mario', 'Draghi'),
+('Barry', 'Allen');
 
 
 INSERT INTO prestiti (isbn, id_utente)
@@ -27,4 +30,5 @@ VALUES ('9788845240000', 5),
 ('9788845240000', 3),
 ('9788823521234', 1),
 ('9788880085096', 2);
+('9788817115061', 6);
 
