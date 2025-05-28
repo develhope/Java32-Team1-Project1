040938fd7f373da7a919580ddcdcf7c25961fb05
Margherita Ravanello <margherita.ravanello@gmail.com>
2025-05-28 16:27:00 +0200

    aggiunta nomi e cognomi in LibroGenerator

--- biblioteca/src/main/java/com/biblioteca/utils/LibroGenerator.java
+++ biblioteca/src/main/java/com/biblioteca/utils/LibroGenerator.java
@@ -10,110 +10,15 @@ import java.util.Random;
 public class LibroGenerator {
 
     static Random random = new Random(2);
-    static List<String> nomi = List.of("Luigi","Sabrina","Anna");
-    static List<String> cognomi = List.of("Bianchi","Gialli","Verdi");
-    static List<String> words = List.of(
-            "defensive",
-            "inspire",
-            "debate",
-            "replace",
-            "crop",
-            "circumstance",
-            "your",
-            "music",
-            "gas",
-            "free",
-            "photograph",
-            "snap",
-            "poll",
-            "which",
-            "designer",
-            "shot",
-            "Mr",
-            "learning",
-            "beach",
-            "sex",
-            "widely",
-            "chemical",
-            "or",
-            "decide",
-            "no",
-            "concern",
-            "heat",
-            "final",
-            "boat",
-            "province",
-            "active",
-            "originally",
-            "four",
-            "wet",
-            "radical",
-            "small",
-            "organize",
-            "shopping",
-            "championship",
-            "finish",
-            "creation",
-            "craft",
-            "veteran",
-            "participate",
-            "tree",
-            "frequently",
-            "figure",
-            "reveal",
-            "PC",
-            "blow",
-            "letter",
-            "description",
-            "affair",
-            "peer",
-            "industrial",
-            "add",
-            "fantasy",
-            "frustration",
-            "bury",
-            "cooking",
-            "tobacco",
-            "incentive",
-            "good",
-            "test",
-            "concentration",
-            "contract",
-            "with",
-            "perception",
-            "rough",
-            "member",
-            "rule",
-            "find",
-            "mainly",
-            "recognize",
-            "job",
-            "per",
-            "gray",
-            "generate",
-            "sanction",
-            "clinical",
-            "knee",
-            "raw",
-            "difficult",
-            "approach",
-            "read",
-            "cheap",
-            "trail",
-            "limited",
-            "tomato",
-            "resistance",
-            "destroy",
-            "voice",
-            "founder",
-            "spending",
-            "mm-hmm");
+    static List<String> nomi = List.of("Luigi", "Sabrina", "Anna", "Marco", "Giulia", "Francesco", "Elisa", "Alessandro", "Chiara", "Matteo", "Martina", "Lorenzo", "Federica", "Davide", "Valentina", "Simone", "Laura", "Gabriele", "Ilaria", "Stefano", "Alessia", "Nicola", "Camilla", "Andrea", "Serena");
+    static List<String> cognomi = List.of("Bianchi", "Gialli", "Verdi", "Rossi", "Neri", "Ferrari", "Russo", "Esposito", "Romano", "Colombo", "Ricci", "Marino", "Greco", "Bruno", "Gallo", "Conti", "Mancini", "Costa", "Giordano", "Rizzo", "Lombardi", "Moretti", "Barbieri", "Fontana", "Santoro");
+    static List<String> words = List.of("defensive", "inspire", "debate", "replace", "crop", "circumstance", "your", "music", "gas", "free", "photograph", "snap", "poll", "which", "designer", "shot", "Mr", "learning", "beach", "sex", "widely", "chemical", "or", "decide", "no", "concern", "heat", "final", "boat", "province", "active", "originally", "four", "wet", "radical", "small", "organize", "shopping", "championship", "finish", "creation", "craft", "veteran", "participate", "tree", "frequently", "figure", "reveal", "PC", "blow", "letter", "description", "affair", "peer", "industrial", "add", "fantasy", "frustration", "bury", "cooking", "tobacco", "incentive", "good", "test", "concentration", "contract", "with", "perception", "rough", "member", "rule", "find", "mainly", "recognize", "job", "per", "gray", "generate", "sanction", "clinical", "knee", "raw", "difficult", "approach", "read", "cheap", "trail", "limited", "tomato", "resistance", "destroy", "voice", "founder", "spending", "mm-hmm");
 
     public static void main(String[] args) {
         LibroGenerator lg = new LibroGenerator();
 
         List<Libro> lista = lg.generaLibri(10);
-        for(Libro l : lista){
+        for (Libro l : lista) {
             System.out.println(lg.generaInsert(l));
         }
         //System.out.println(lg.generaLibri(10));
@@ -128,9 +33,7 @@ public class LibroGenerator {
     }
 
     public Libro generaLibro() {
-        return new Libro(generaTitolo(),
-                generaAutore(),generaAnno(),
-                generaIsbn(), generaNumeroCopie());
+        return new Libro(generaTitolo(), generaAutore(), generaAnno(), generaIsbn(), generaNumeroCopie());
     }
 
     public String generaAutore() {
@@ -141,7 +44,7 @@ public class LibroGenerator {
     }
 
     public String generaIsbn() {
-        return String.valueOf(random.nextLong(100000000000l,999999999999l));
+        return String.valueOf(random.nextLong(100000000000L, 999999999999L));
     }
 
     public int generaAnno() {
@@ -150,23 +53,21 @@ public class LibroGenerator {
 
     public String generaTitolo() {
 
-        int numeroParole = random.nextInt(2,5);
+        int numeroParole = random.nextInt(2, 5);
         StringBuilder titolo = new StringBuilder();
-        for(int i = 0; i<numeroParole; i++){
-           String parola = words.get(random.nextInt(words.size()));
-           titolo.append(" " + parola);
+        for (int i = 0; i < numeroParole; i++) {
+            String parola = words.get(random.nextInt(words.size()));
+            titolo.append(" " + parola);
         }
         return titolo.toString().trim();
     }
 
     public int generaNumeroCopie() {
-        return random.nextInt(0,100);
+        return random.nextInt(0, 100);
     }
 
-    public String generaInsert(Libro l){
-        return  "INSERT INTO libri (titolo, autore, anno_pubblicazione,isbn, numero_copie) " +
-                        "VALUES ( '"+l.getTitolo()+"','" +l.getAutore()+"',"+l.getAnnoPubblicazione()
-                            +",'"+l.getISBN()+"'," +l.getNumeroCopie()+");";
+    public String generaInsert(Libro l) {
+        return "INSERT INTO libri (titolo, autore, anno_pubblicazione,isbn, numero_copie) " + "VALUES ( '" + l.getTitolo() + "','" + l.getAutore() + "'," + l.getAnnoPubblicazione() + ",'" + l.getISBN() + "'," + l.getNumeroCopie() + ");";
 
 
     }
